8d67ae5b8c249e9eaecd08ecc7575cda4c861d9b
Erick Bernardo <erickvictor@gmail.com>
2018-06-29 21:01:07 -0400

    Add coverage

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 const FizzBuzz = (num) => {
+  if (num === 0) return 0;
   if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
   if (num % 3 === 0) return 'Fizz';
   if (num % 5 === 0) return 'Buzz';
